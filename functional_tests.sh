405fb83eefad07180d853f8931e9bc3a8853847e
John Murphy <murphybytes@gmail.com>
2012-01-08 21:04:01 -0600

    more work on tests

--- functional_tests.sh
+++ functional_tests.sh
@@ -7,22 +7,15 @@
 #
 ################################################
 
-function usage() {
-    echo "Script failed. Run sudo test_setup.sh first." 
-    exit 1
-}
 
-if [ -z ${DUMMY_ACCOUNT} ]; then
-    usage
+
+if [ -z "${DUMMY_ACCOUNT}" ]; then
+    echo "Script failed. Run sudo test_setup.sh first." 
     exit 1
 fi
 
 
-CURRENT_USER=$(whoami)
-
-echo "Current users ${CURRENT_USER}"
-
-sudo su - ${DUMMY_ACCOUNT}
+sudo su --login -c "./ucp --verbose -d -L --pid-directory=/home/${DUMMY_ACCOUNT} > trace.log &"  $DUMMY_ACCOUNT
 
 
 
